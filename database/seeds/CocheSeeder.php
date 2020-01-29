436aa73f2536fdcc0198061147839c568fe6837e
RubenGarciaGonzalez <rubengg_00@hotmail.com>
2020-01-29 11:14:39 +0100

    seeders

--- database/seeds/CocheSeeder.php
+++ database/seeds/CocheSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Coche;
 
 class CocheSeeder extends Seeder
 {
@@ -11,6 +13,73 @@ class CocheSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Desactivamos las constraints
+        DB::statement("SET FOREIGN_KEY_CHECKS=0;");
+        DB::table('coches')->truncate();
+        DB::statement("SET FOREIGN_KEY_CHECKS=1;");
+
+        Coche::create([
+            'matricula'=>'1234-JJJ',
+            'modelo'=>'Leon',
+            'color'=>'Rojo',
+            'tipo'=>'Diesel',
+            'klms'=>'23000',
+            'pvp'=>'14000',
+            'marca_id'=>'1'
+        ]);
+
+        Coche::create([
+            'matricula'=>'2312-SFK',
+            'modelo'=>'Golf',
+            'color'=>'Azul',
+            'tipo'=>'Gasolina',
+            'klms'=>'3260',
+            'pvp'=>'21000',
+            'marca_id'=>'2'
+        ]);
+
+        Coche::create([
+            'matricula'=>'5235-ASD',
+            'modelo'=>'Punto',
+            'color'=>'Gris',
+            'tipo'=>'Diesel',
+            'klms'=>'40000',
+            'pvp'=>'12000',
+            'marca_id'=>'3'
+        ]);
+
+        Coche::create([
+            'matricula'=>'1341-TRD',
+            'modelo'=>'Clio',
+            'color'=>'Amarillo',
+            'tipo'=>'Diesel',
+            'klms'=>'15340',
+            'pvp'=>'9000',
+            'marca_id'=>'4'
+        ]);
+
+        Coche::create([
+            'matricula'=>'1387-TTD',
+            'modelo'=>'Megane',
+            'color'=>'Verde',
+            'tipo'=>'Gasolina',
+            'klms'=>'54240',
+            'pvp'=>'10500',
+            'marca_id'=>'4'
+        ]);
+
+
+        Coche::create([
+            'matricula'=>'6346-FOP',
+            'modelo'=>'Astra',
+            'color'=>'Blanco',
+            'tipo'=>'Gasolina',
+            'klms'=>'48709',
+            'pvp'=>'9500',
+            'marca_id'=>'5'
+        ]);
+
+
+
     }
 }
--- database/seeds/MarcaSeeder.php
+++ database/seeds/MarcaSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Marca;
 
 class MarcaSeeder extends Seeder
 {
@@ -11,6 +12,36 @@ class MarcaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Desactivamos las constraints
+        DB::statement("SET FOREIGN_KEY_CHECKS=0;");
+        DB::table('marcas')->truncate();
+        DB::statement("SET FOREIGN_KEY_CHECKS=1;");
+
+
+        Marca::create([
+                'nombre'=>'Seat',
+                'pais'=>'España'
+        ]);
+
+        Marca::create([
+            'nombre'=>'Volkswagen',
+            'pais'=>'Alemania'
+        ]);
+
+        Marca::create([
+            'nombre'=>'Fiat',
+            'pais'=>'Italia'
+        ]);
+
+        Marca::create([
+            'nombre'=>'Renault',
+            'pais'=>'Francia'
+        ]);
+
+        Marca::create([
+            'nombre'=>'Opel',
+            'pais'=>'Alemania'
+        ]);
+
     }
 }
